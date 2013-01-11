e3d682c27d1194858674c7f0863fef83ed34ecf8
George Reese <george.reese@imaginary.com>
2013-01-11 10:06:55 -0600

    Added volume scaling options (issue #30)

--- src/main/java/org/dasein/cloud/compute/VMScalingCapabilities.java
+++ src/main/java/org/dasein/cloud/compute/VMScalingCapabilities.java
@@ -16,26 +16,56 @@
  */
 package org.dasein.cloud.compute;
 
+import org.dasein.cloud.Requirement;
+
+import javax.annotation.Nonnull;
+
 /**
  * Describes the ways in which a cloud can support vertical scaling.
  * <p>Created by George Reese: 11/18/12 7:57 PM</p>
  * @author George Reese
  * @version 2013.01 initial version (Issue #9)
+ * @version 2013.02 added support for new volumes/volume resizing as a VM alteration (issue #30)
  * @since 2013.01
  */
 public class VMScalingCapabilities {
-    static public VMScalingCapabilities getInstance(boolean newVm, boolean product) {
+    static public VMScalingCapabilities getInstance(boolean newVm, boolean product, @Nonnull Requirement alterVmForNewVolume, @Nonnull Requirement alterVmForVolumeChange) {
         VMScalingCapabilities capabilities = new VMScalingCapabilities();
 
         capabilities.createsNewVirtualMachine = newVm;
         capabilities.supportsProductChanges = product;
+        capabilities.alterVmForNewVolume = alterVmForNewVolume;
+        capabilities.alterVmForVolumeChange = alterVmForVolumeChange;
         return capabilities;
     }
 
-    private boolean createsNewVirtualMachine;
-    private boolean supportsProductChanges;
+    private Requirement alterVmForNewVolume;
+    private Requirement alterVmForVolumeChange;
+    private boolean     createsNewVirtualMachine;
+    private boolean     supportsProductChanges;
 
     private VMScalingCapabilities() { }
+
+    /**
+     * Indicates whether or not you must alter a virtual machine in order to attach new volumes to virtual machines.
+     * If this is not required and the cloud supports volume attachment, then you simply use the normal
+     * {@link VolumeSupport#attach(String, String, String)} method. If it is required, however, you use
+     * {@link VirtualMachineSupport#alterVirtualMachine(String, VMScalingOptions)}.
+     * @return the requirement for handling new volumes as an alter VM operation
+     */
+    public @Nonnull Requirement getAlterVmForNewVolume() {
+        return alterVmForNewVolume;
+    }
+
+    /**
+     * Indicates whether or not you must alter a virtual machine in order to resize attached volumes. If
+     * {@link Requirement#NONE}, then either you cannot resize volumes or you must do that through mechanisms other
+     * than {@link VirtualMachineSupport#alterVirtualMachine(String, VMScalingOptions)}.
+     * @return the requirement for handling volume resizing as an alter VM operation
+     */
+    public @Nonnull Requirement getAlterVmForVolumeChange() {
+        return alterVmForVolumeChange;
+    }
 
     /**
      * Indicates that vertical scaling will result in a new virtual machine with a new unique ID to replace the
--- src/main/java/org/dasein/cloud/compute/VMScalingOptions.java
+++ src/main/java/org/dasein/cloud/compute/VMScalingOptions.java
@@ -16,14 +16,20 @@
  */
 package org.dasein.cloud.compute;
 
+import org.dasein.util.uom.storage.Gigabyte;
+import org.dasein.util.uom.storage.Storage;
+
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Options for vertical scaling of a running virtual machine.
  * <p>Created by George Reese: 11/18/12 7:52 PM</p>
  * @author George Reese
  * @version 2013.01 initial version (Issue #9)
+ * @version 2013.02 added volume scaling options (issue #30)
  * @since 2013.01
  */
 public class VMScalingOptions {
@@ -39,7 +45,9 @@ public class VMScalingOptions {
         return options;
     }
 
-    private String providerProductId;
+    private String                        providerProductId;
+    private VolumeCreateOptions[]         volumesToCreate;
+    private Storage<Gigabyte>[]           volumesToResize;
 
     private VMScalingOptions() { }
 
@@ -48,5 +56,45 @@ public class VMScalingOptions {
      */
     public @Nullable String getProviderProductId() {
         return providerProductId;
+    }
+
+    /**
+     * @return the create options for the volumes to create an attach as part of this alteration process
+     */
+    public @Nonnull VolumeCreateOptions[] getVolumesToCreate() {
+        return (volumesToCreate == null ? new VolumeCreateOptions[0] : volumesToCreate);
+    }
+
+    /**
+     * @return the volumes to resize as part of the alteration process
+     */
+    public @Nonnull Storage<Gigabyte>[] getVolumesToResize() {
+        if( volumesToResize == null ) {
+            return (Storage<Gigabyte>[])(new Storage[0]);
+        }
+        return volumesToResize;
+    }
+
+    /**
+     * Identifies that this scaling operation will create volumes to be attached during the scale.
+     * @param options the list of options for creating the new volumes
+     * @return this
+     */
+    public @Nonnull VMScalingOptions withVolumesToCreate(@Nonnull VolumeCreateOptions ... options) {
+        volumesToCreate = options;
+        return this;
+    }
+
+    /**
+     * Indicates that one or more of the volumes currently attached to the VM will be resized. The match is done by
+     * index. If a volume is not to be resized, it should have a null entry in this call. The number of volumes in
+     * this call may be less than the number of volumes attached to the VM. In that case, the extra volumes are to
+     * be unchanged during the alter VM process.
+     * @param volumes the new sizes of the volumes attached to the VM with null entries indicating no change
+     * @return this
+     */
+    public @Nonnull VMScalingOptions withVolumeToResize(@Nonnull Storage<Gigabyte> ... volumes) {
+        volumesToResize = volumes;
+        return this;
     }
 }
