1086a544bdd6a3ebac5a573a7a3b56140e7f655c
George Reese <george.reese@imaginary.com>
2013-01-15 10:16:55 -0600

    Added more javadoc

--- src/main/java/org/dasein/cloud/compute/ComputeServices.java
+++ src/main/java/org/dasein/cloud/compute/ComputeServices.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2009-2012 enStratus Networks Inc.
+ * Copyright (C) 2009-2013 enStratus Networks Inc.
  *
  * ====================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,24 +20,62 @@ package org.dasein.cloud.compute;
 
 import javax.annotation.Nullable;
 
+/**
+ * Compute services define how a consuming application interacts with cloud compute as a service resources like
+ * virtual machines, volumes, and images/templates. To use this (or any other services class), you call one of
+ * the <code>getXXXSupport()</code> methods to find the support class for a specific resource. If the method
+ * returns null, the cloud does not have support for that kind of resource.
+ * @version 2013.02 added javadoc
+ * @since unknown
+ */
 public interface ComputeServices {
+    /**
+     * @return access to support for auto-scaling capabilities native to the cloud provider
+     */
     public abstract @Nullable AutoScalingSupport getAutoScalingSupport();
-    
+
+    /**
+     * @return access to support for images/templates in the cloud provider
+     */
     public abstract @Nullable MachineImageSupport getImageSupport();
-    
+
+    /**
+     * @return access to support for volume snapshots in the cloud provider
+     */
     public abstract @Nullable SnapshotSupport getSnapshotSupport();
-    
+
+    /**
+     * @return access to support for virtual machines in the cloud provider
+     */
     public abstract @Nullable VirtualMachineSupport getVirtualMachineSupport();
-    
+
+    /**
+     * @return access to support for volumes in the cloud provider
+     */
     public abstract @Nullable VolumeSupport getVolumeSupport();
-    
+
+    /**
+     * @return indicates whether or not the cloud provider supports native auto-scaling capabilities
+     */
     public abstract boolean hasAutoScalingSupport();
-    
+
+    /**
+     * @return indicates whether or not the cloud provider supports images/templates
+     */
     public abstract boolean hasImageSupport();
-    
+
+    /**
+     * @return indicates whether or not the cloud provider supports snapshotting volumes
+     */
     public abstract boolean hasSnapshotSupport();
-    
+
+    /**
+     * @return indicates whether or not the cloud provider supports virtual machines
+     */
     public abstract boolean hasVirtualMachineSupport();
-    
+
+    /**
+     * @return indicates whether or not the cloud provider supports block or network volumes
+     */
     public abstract boolean hasVolumeSupport();
 }
--- src/main/java/org/dasein/cloud/compute/VolumeCreateOptions.java
+++ src/main/java/org/dasein/cloud/compute/VolumeCreateOptions.java
@@ -18,6 +18,8 @@
 
 package org.dasein.cloud.compute;
 
+import org.dasein.cloud.Requirement;
+import org.dasein.cloud.dc.DataCenter;
 import org.dasein.util.uom.storage.Gigabyte;
 import org.dasein.util.uom.storage.Storage;
 
@@ -26,21 +28,52 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 /**
- * Configuration options for creating volumes.
+ * Configuration options for creating volumes. When you create volumes, you provide an instance of this class to
+ * indicate the different configuration options for instantiating the volume. Not all clouds or volume types require
+ * all values to be specified. Use the meta-data in {@link VolumeSupport} to programmatically determine what is
+ * required.
  * <p>Created by George Reese: 6/22/12 8:41 PM</p>
  * @author George Reese (george.reese@imaginary.com)
  * @since 2012-07
  * @version 2012-07 introduced volume create options
+ * @version 2013.02 added javadoc
  */
 public class VolumeCreateOptions {
+    /**
+     * Provides options for creating a block volume of a specific size. Useful only when {@link VolumeSupport#getVolumeProductRequirement()}
+     * is {@link Requirement#NONE} or {@link Requirement#OPTIONAL}.
+     * @param size the size of the volume to be created (or to be resized to)
+     * @param name the name of the new volume
+     * @param description a friendly description of the purpose of the new volume
+     * @return an object representing the options for creating the new volume
+     */
     static public VolumeCreateOptions getInstance(@Nonnull Storage<?> size, @Nonnull String name, @Nonnull String description) {
         return new VolumeCreateOptions(null, null, size, name, description, 0);
     }
 
+    /**
+     * Provides options for creating a block volume of a specific size based on a specific product. This method makes no sense
+     * when {@link VolumeSupport#getVolumeProductRequirement()}  is {@link Requirement#NONE}.
+     * @param volumeProductId the ID of the volume product from {@link VolumeSupport#listVolumeProducts()} to use in creating the volume
+     * @param size the size of the volume to be created (or to be resized to)
+     * @param name the name of the new volume
+     * @param description a friendly description of the purpose of the new volume
+     * @param iops the minimum guaranteed iops or 0 if no guarantees are sought
+     * @return an object representing the options for creating the new volume
+     */
     static public VolumeCreateOptions getInstance(@Nonnull String volumeProductId, @Nonnull Storage<?> size, @Nonnull String name, @Nonnull String description, @Nonnegative int iops) {
         return new VolumeCreateOptions(volumeProductId, null, size, name, description, iops);
     }
 
+    /**
+     * Provides options for creating a network volume of a specific size. Useful only when {@link VolumeSupport#getVolumeProductRequirement()}
+     * is {@link Requirement#NONE} or {@link Requirement#OPTIONAL}.
+     * @param inVlanId the ID of the VLAN in which the network volume will be created
+     * @param size the size of the volume to be created (or to be resized to)
+     * @param name the name of the new volume
+     * @param description a friendly description of the purpose of the new volume
+     * @return an object representing the options for creating the new volume
+     */
     static public VolumeCreateOptions getNetworkInstance(@Nonnull String inVlanId, @Nonnull Storage<?> size, @Nonnull String name, @Nonnull String description) {
         VolumeCreateOptions options = new VolumeCreateOptions(null, null, size, name, description, 0);
 
@@ -49,6 +82,16 @@ public class VolumeCreateOptions {
         return options;
     }
 
+    /**
+     * Provides options for creating a network volume of a specific size based on a specific product. This method makes no sense
+     * when {@link VolumeSupport#getVolumeProductRequirement()} is {@link Requirement#NONE}.
+     * @param inVlanId the ID of the VLAN in which the network volume will be created
+     * @param volumeProductId the ID of the volume product from {@link VolumeSupport#listVolumeProducts()} to use in creating the volume
+     * @param size the size of the volume to be created (or to be resized to)
+     * @param name the name of the new volume
+     * @param description a friendly description of the purpose of the new volume
+     * @return an object representing the options for creating the new volume
+     */
     static public VolumeCreateOptions getNetworkInstance(@Nonnull String volumeProductId, @Nonnull String inVlanId, @Nonnull Storage<?> size, @Nonnull String name, @Nonnull String description) {
         VolumeCreateOptions options = new VolumeCreateOptions(volumeProductId, null, size, name, description, 0);
 
@@ -57,6 +100,17 @@ public class VolumeCreateOptions {
         return options;
     }
 
+    /**
+     * Provides options for creating a network volume of a specific size based on a specific product. This method makes no sense
+     * when {@link VolumeSupport#getVolumeProductRequirement()} is {@link Requirement#NONE}.
+     * @param inVlanId the ID of the VLAN in which the network volume will be created
+     * @param volumeProductId the ID of the volume product from {@link VolumeSupport#listVolumeProducts()} to use in creating the volume
+     * @param size the size of the volume to be created (or to be resized to)
+     * @param name the name of the new volume
+     * @param description a friendly description of the purpose of the new volume
+     * @param iops the minimum guaranteed iops or 0 if no guarantees are sought
+     * @return an object representing the options for creating the new volume
+     */
     static public VolumeCreateOptions getNetworkInstance(@Nonnull String volumeProductId, @Nonnull String inVlanId, @Nonnull Storage<?> size, @Nonnull String name, @Nonnull String description, @Nonnegative int iops) {
         VolumeCreateOptions options = new VolumeCreateOptions(volumeProductId, null, size, name, description, iops);
 
@@ -65,10 +119,32 @@ public class VolumeCreateOptions {
         return options;
     }
 
+    /**
+     * Provides options for creating a block volume from a snapshot. This method makes no sense when {@link ComputeServices#getSnapshotSupport()}
+     * is <code>null</code>. Useful only when {@link VolumeSupport#getVolumeProductRequirement()}
+     * is {@link Requirement#NONE} or {@link Requirement#OPTIONAL}.
+     * @param snapshotId the ID of the snapshot from which the volume will be created
+     * @param size the size of the volume to be created
+     * @param name the name of the volume to be created
+     * @param description a friendly description of the purpose of the new volume
+     * @return an object representing the options for creating the new volume
+     */
     static public VolumeCreateOptions getInstanceForSnapshot(@Nonnull String snapshotId, @Nonnull Storage<?> size, @Nonnull String name, @Nonnull String description) {
         return new VolumeCreateOptions(null, snapshotId, size, name, description, 0);
     }
 
+    /**
+     * Provides options for creating a block volume from a snapshot. This method makes no sense when {@link ComputeServices#getSnapshotSupport()}
+     * is <code>null</code>. Useful only when {@link VolumeSupport#getVolumeProductRequirement()} is <em>NOT</em>
+     * {@link Requirement#NONE}.
+     * @param volumeProductId the ID of the volume product from which the new volume will be created
+     * @param snapshotId the ID of the snapshot from which the volume will be created
+     * @param size the size of the volume to be created
+     * @param name the name of the volume to be created
+     * @param description a friendly description of the purpose of the new volume
+     * @param iops the guaranteed iops for the new volume (or 0 for no guarantee)
+     * @return an object representing the options for creating the new volume
+     */
     static public VolumeCreateOptions getInstanceForSnapshot(@Nonnull String volumeProductId, @Nonnull String snapshotId, @Nonnull Storage<?> size, @Nonnull String name, @Nonnull String description, @Nonnegative int iops) {
         return new VolumeCreateOptions(volumeProductId, snapshotId, size, name, description, iops);
     }
@@ -98,55 +174,102 @@ public class VolumeCreateOptions {
         this.format = VolumeFormat.BLOCK;
     }
 
+    /**
+     * @return the data center into which the new volume will be provisioned
+     */
     public @Nullable String getDataCenterId() {
         return dataCenterId;
     }
-    
+
+    /**
+     * @return a friendly description of the function of the volume to be created
+     */
     public @Nonnull String getDescription() {
         return description;
     }
 
+    /**
+     * @return the device ID under which the volume will be attached
+     */
     public @Nullable String getDeviceId() {
         return deviceId;
     }
 
+    /**
+     * @return the format for the requested volume
+     */
     public @Nonnull VolumeFormat getFormat() {
         return format;
     }
 
+    /**
+     * Provides the requested minimum IOPS. This value will be ignored in clouds that cannot honor it.
+     * @return a minimum IOPS guarantee for this volume, or 0 for no guarantees
+     */
     public @Nonnegative int getIops() {
         return iops;
     }
 
+    /**
+     * @return a friendly (and ideally unique) name for this value for display in different contexts
+     */
     public @Nonnull String getName() {
         return name;
     }
-    
+
+    /**
+     * @return the unique ID of the {@link Snapshot} from which this volume will be created
+     */
     public @Nullable String getSnapshotId() {
         return snapshotId;
     }
-    
+
+    /**
+     * @return the virtual machine to which this volume will be automatically attached post-create.
+     */
     public @Nullable String getVirtualMachineId() {
         return virtualMachineId;
     }
 
+    /**
+     * @return the VLAN in which the volume will be provisioned (typically specified for network volumes)
+     */
     public @Nullable String getVlanId() {
         return vlanId;
     }
 
+    /**
+     * @return the unique ID of the {@link VolumeProduct} to use in creating a new volume
+     */
     public @Nullable String getVolumeProductId() {
         return volumeProductId;
     }
-    
+
+    /**
+     * A cloud should honor the size <em>as best as possible</em> when a request is made to create a volume. The
+     * Dasein implementation will favor creating a volume over exactly matching the requested size.
+     * @return the expected size of the volume to be created
+     */
     public @Nonnull Storage<Gigabyte> getVolumeSize() {
         return volumeSize;
     }
 
+    /**
+     * Specifies the data center in which the volume will be provisioned
+     * @param dataCenterId the unique ID of the {@link DataCenter} in which the volume will be provisioned
+     * @return this
+     */
     public @Nonnull VolumeCreateOptions inDataCenter(@Nonnull String dataCenterId) {
         this.dataCenterId = dataCenterId;
         return this;
     }
 
+    /**
+     * Indicates how the volume will be attached to a specific virtual machine on create.
+     * @param vmId the unique ID of the {@link VirtualMachine} to which the new volume will be automatically attached after create
+     * @param asDeviceId the device ID (see {@link VolumeSupport#listPossibleDeviceIds(Platform)} for valid device IDs) under which it will be attached
+     * @return this
+     */
     public @Nonnull VolumeCreateOptions withAttachment(@Nonnull String vmId, @Nonnull String asDeviceId) {
         this.virtualMachineId = vmId;
         this.deviceId = asDeviceId;
