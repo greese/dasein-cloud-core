1c034950ecbc54323eaf3cf3099dd9492115d6c3
Chris Kelner <chris.kelner@gmail.com>
2013-08-09 11:05:45 -0400

    give an explaination for org.dasein.cloud.compute.alarm
    Closes #896

--- src/main/java/org/dasein/cloud/compute/Alarm.java
+++ src/main/java/org/dasein/cloud/compute/Alarm.java
@@ -1,10 +1,19 @@
-/*
-  A simple alarm to support AutoScaling...
- */
-
 package org.dasein.cloud.compute;
 
 import java.io.Serializable;
+
+/**
+ * <p>
+ * Terse alarm class for use inside other resources.
+ * Specific scenario currently documented for AWS where calling 'DescribePolicies'
+ * for autoscaling: http://docs.aws.amazon.com/AutoScaling/latest/APIReference/API_DescribePolicies.html
+ * This call returns an object (http://docs.aws.amazon.com/AutoScaling/latest/APIReference/API_ScalingPolicy.html)
+ * with an alarm list that consist only of name and ARN: http://docs.aws.amazon.com/AutoScaling/latest/APIReference/API_Alarm.html
+ * </p>
+ *
+ * @author Chris Kelner (chris.kelner@weather.com) - [http://github.com/ckelner]
+ *
+ */
 
 public class Alarm implements Serializable {
     private static final long serialVersionUID = 4416279768844752736L;
