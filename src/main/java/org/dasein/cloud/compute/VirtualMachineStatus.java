66cdc28e21bd43b8dfcfc2f4b2258bb256ef0ba3
Chris Kelner <chris.kelner@gmail.com>
2014-02-14 17:09:40 -0500

    strip down VirtualMachineStatus to something more simple
    Ref TheWeatherCompany/grid-api#509

--- src/main/java/org/dasein/cloud/compute/VirtualMachineStatus.java
+++ src/main/java/org/dasein/cloud/compute/VirtualMachineStatus.java
@@ -21,51 +21,15 @@ package org.dasein.cloud.compute;
 
 /**
  * <p>
- * A slimmed down version of VirtualMachine that only pertains to status of the machine.
+ * Host and Server status for a given Virtual Machine
  * </p>
  */
 public class VirtualMachineStatus {
-    private VmState               currentState;
-    private String                providerDataCenterId;
-    private String                providerOwnerId;
-    private String                providerRegionId;
     private String                providerVirtualMachineId;
     private VmStatus              providerHostStatus;
     private VmStatus              providerVmStatus;
 
   public VirtualMachineStatus() { }
-
-    public VmState getCurrentState() {
-        return currentState;
-    }
-
-    public void setCurrentState(VmState currentState) {
-        this.currentState = currentState;
-    }
-
-    public String getProviderDataCenterId() {
-        return providerDataCenterId;
-    }
-
-    public void setProviderDataCenterId(String providerDataCenterId) {
-        this.providerDataCenterId = providerDataCenterId;
-    }
-
-    public String getProviderOwnerId() {
-        return providerOwnerId;
-    }
-
-    public void setProviderOwnerId(String providerOwnerId) {
-        this.providerOwnerId = providerOwnerId;
-    }
-
-    public String getProviderRegionId() {
-        return providerRegionId;
-    }
-
-    public void setProviderRegionId(String providerRegionId) {
-        this.providerRegionId = providerRegionId;
-    }
 
     public String getProviderVirtualMachineId() {
         return providerVirtualMachineId;
