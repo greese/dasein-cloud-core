5fc8d392e5985251b96f18c23e45aa6c8017af65
Stas Maksimov <maksimov@gmail.com>
2014-05-22 21:58:34 +0100

    Some updates to HC matching; fixed forgotten doubles for timeout and interval;

--- src/main/java/org/dasein/cloud/network/HealthCheckOptions.java
+++ src/main/java/org/dasein/cloud/network/HealthCheckOptions.java
@@ -90,7 +90,7 @@ public class HealthCheckOptions{
         return options;
     }
 
-    public @Nonnull LoadBalancerHealthCheck build(@Nonnull CloudProvider provider, @Nullable String providerLoadBalancerId) throws CloudException, InternalException {
+    public @Nonnull LoadBalancerHealthCheck build(@Nonnull CloudProvider provider) throws CloudException, InternalException {
         NetworkServices services = provider.getNetworkServices();
 
         if( services == null ) {
@@ -113,7 +113,7 @@ public class HealthCheckOptions{
         this.name = name;
     }
 
-    public String getDescription() {
+    public @Nullable String getDescription() {
         return description;
     }
 
@@ -129,7 +129,7 @@ public class HealthCheckOptions{
         this.providerLoadBalancerId = providerLoadBalancerId;
     }
 
-    public String getHost() {
+    public @Nullable String getHost() {
         return host;
     }
 
@@ -153,7 +153,7 @@ public class HealthCheckOptions{
         this.port = port;
     }
 
-    public String getPath() {
+    public @Nullable String getPath() {
         return path;
     }
 
