93106c8cff55374026261ae92d87f78a250668f9
Roger Unwin <roger.unwin@enstratius.com>
2014-06-03 08:22:15 -0700

    fixed getInstance not passing some params it was given.

--- src/main/java/org/dasein/cloud/network/LoadBalancerHealthCheck.java
+++ src/main/java/org/dasein/cloud/network/LoadBalancerHealthCheck.java
@@ -61,11 +61,11 @@ public class LoadBalancerHealthCheck implements Networkable{
     }
 
     public static LoadBalancerHealthCheck getInstance(@Nullable String providerLBHealthCheckId, @Nonnull String name, @Nullable String description, @Nullable String host, @Nullable HCProtocol protocol, int port, @Nullable String path, int interval, int timeout, int healthyCount, int unhealthyCount){
-        return new LoadBalancerHealthCheck(providerLBHealthCheckId, name, description, host, null, port, path, interval, timeout, unhealthyCount, healthyCount);
+        return new LoadBalancerHealthCheck(providerLBHealthCheckId, name, description, host, protocol, port, path, interval, timeout, unhealthyCount, healthyCount);
     }
 
     private LoadBalancerHealthCheck(@Nullable String providerLBHealthCheckId, @Nullable String name, @Nullable String description, @Nullable String host, @Nullable HCProtocol protocol, int port, @Nullable String path, int interval, int timeout, int healthyCount, int unhealthyCount){
-        this.providerLBHealthCheckId = providerLBHealthCheckId;
+    	this.providerLBHealthCheckId = providerLBHealthCheckId;
         this.name = name;
         this.description = description;
         this.host = host;
