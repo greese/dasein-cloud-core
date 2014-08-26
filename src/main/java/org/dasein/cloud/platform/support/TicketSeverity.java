95a5f49bc785ce8edc5fe53f23157dc66cd72559
Eugene Yaroslavtsev <eyaroslavtsev@evelopers.com>
2014-08-26 15:04:55 +0400

    valueOf fix of ticketSeverity (#1106)

--- src/main/java/org/dasein/cloud/platform/support/TicketSeverity.java
+++ src/main/java/org/dasein/cloud/platform/support/TicketSeverity.java
@@ -1,5 +1,7 @@
 package org.dasein.cloud.platform.support;
 
+import org.dasein.cloud.CloudException;
+
 /**
  * @author Eugene Yaroslavtsev
  * @since 26.08.2014
@@ -18,6 +20,22 @@ public enum TicketSeverity {
         this.num = num;
     }
 
+    public static TicketSeverity valueOf(Object value) throws CloudException {
+        if (value != null && isString(value)) {
+            TicketSeverity validateValue = validateValue(String.valueOf(value));
+            if (validateValue != null) {
+                return validateValue;
+            } else {
+                for (TicketSeverity severity : values()) {
+                    if (severity.name().equalsIgnoreCase(String.valueOf(value))) {
+                        return severity;
+                    }
+                }
+            }
+        }
+        throw new CloudException("Invalid value for TicketSeverity " + value);
+    }
+
     public static TicketSeverity valueByPriority(Integer value) {
         for (TicketSeverity severity : values()) {
             if (severity.num.equals(value)) {
@@ -25,5 +43,19 @@ public enum TicketSeverity {
             }
         }
         return UNKNOWN;//default
+    }
+
+    public static boolean isString(Object o) {
+        return o.getClass().equals(String.class);
+    }
+
+    public static TicketSeverity validateValue(String value) {
+        try {
+            return valueOf(TicketSeverity.class, value);
+        } catch (IllegalArgumentException ex) {
+            return null;
+        } catch (NullPointerException npe) {
+            return null;
+        }
     }
 }
