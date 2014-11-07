1ba6523636634f7fb138c5ef7ea9f6ca45f00f83
Eugene Yaroslavtsev <eyaroslavtsev@evelopers.com>
2014-11-07 14:44:15 +0300

    remove healthCheck from LoadBalancer (#1298)

--- src/main/java/org/dasein/cloud/network/LoadBalancer.java
+++ src/main/java/org/dasein/cloud/network/LoadBalancer.java
@@ -136,7 +136,6 @@ public class LoadBalancer implements Networkable, Taggable {
     private Map<String,String>      tags;
     private String                  providerLBHealthCheckId;
     private String[]                providerFirewallIds;
-    private LoadBalancerHealthCheck healthCheck;
     private VisibleScope            visibleScope;
 
     /**
@@ -428,14 +427,6 @@ public class LoadBalancer implements Networkable, Taggable {
         }
         Collections.addAll(this.listeners, listeners);
         return this;
-    }
-
-    public LoadBalancerHealthCheck getHealthCheck() {
-        return healthCheck;
-    }
-
-    public void setHealthCheck( LoadBalancerHealthCheck healthCheck ) {
-        this.healthCheck = healthCheck;
     }
 
     /******************************* DEPRECATED METHODS ***********************************/
