96467c475349bf70be4a416542eb7939aa1ee6e7
Stas Maksimov <maksimov@gmail.com>
2014-11-11 20:26:32 +0000

    Added generic type and © to AbstractAutoScalingSupport

--- src/main/java/org/dasein/cloud/compute/AbstractAutoScalingSupport.java
+++ src/main/java/org/dasein/cloud/compute/AbstractAutoScalingSupport.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright (C) 2009-2014 Dell, Inc.
+ * See annotations for authorship information
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ====================================================================
+ */
 package org.dasein.cloud.compute;
 
 import org.dasein.cloud.CloudException;
@@ -15,11 +33,15 @@ import java.util.Collection;
  * User: Eugene Yaroslavtsev
  * Date: 31.07.2014
  */
-public abstract class AbstractAutoScalingSupport implements AutoScalingSupport {
-    private CloudProvider provider;
+public abstract class AbstractAutoScalingSupport<T extends CloudProvider> implements AutoScalingSupport {
+    private T provider;
 
-    public AbstractAutoScalingSupport( @Nonnull CloudProvider provider ) {
+    public AbstractAutoScalingSupport( @Nonnull T provider ) {
         this.provider = provider;
+    }
+
+    protected final @Nonnull T getProvider() {
+        return provider;
     }
 
     @Override
