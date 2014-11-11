9a69aebae2acad6cd7c82a7076485b615e35bb41
Stas Maksimov <maksimov@gmail.com>
2014-11-11 20:27:17 +0000

    Added ©

--- src/main/java/org/dasein/cloud/compute/AbstractAffinityGroupSupport.java
+++ src/main/java/org/dasein/cloud/compute/AbstractAffinityGroupSupport.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright (C) 2009-2014 Dell, Inc.
+ * See annotations for authorship information
+ *
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ====================================================================
+ */
 package org.dasein.cloud.compute;
 
 import org.dasein.cloud.CloudException;
@@ -9,7 +27,7 @@ import org.dasein.cloud.identity.ServiceAction;
 import javax.annotation.Nonnull;
 
 /**
- * Basic default implemetations of all AffinityGroupSupport methods
+ * Basic default implementations of all AffinityGroupSupport methods
  * Created by Drew Lyall: 16/07/14 15:43
  * @author Drew Lyall
  * @version 2014.08
