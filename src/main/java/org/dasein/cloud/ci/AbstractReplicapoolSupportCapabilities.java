3ac4f9e7b3deedfb69f9d21111a3a730f721cb91
Roger Unwin <roger.unwin@enstratius.com>
2015-02-10 14:23:46 -0800

    * Fixed spelling error. * Added start of capabilities

---
+++ src/main/java/org/dasein/cloud/ci/AbstractReplicapoolSupportCapabilities.java
@@ -0,0 +1,9 @@
+package org.dasein.cloud.ci;
+
+import javax.annotation.Nonnull;
+
+import org.dasein.cloud.Capabilities;
+
+public interface AbstractReplicapoolSupportCapabilities extends Capabilities {
+
+}
