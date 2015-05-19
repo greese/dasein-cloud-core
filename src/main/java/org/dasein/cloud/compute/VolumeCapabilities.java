e8d6dea8de22767ff83de85729959471e71c67e3
Stas Maksimov <maksimov@gmail.com>
2015-05-19 19:45:31 +0100

    Renamed the volume capability and updated annos on VolumeSupport#attach() where deviceId is now optional

--- src/main/java/org/dasein/cloud/compute/VolumeCapabilities.java
+++ src/main/java/org/dasein/cloud/compute/VolumeCapabilities.java
@@ -144,12 +144,15 @@ public interface VolumeCapabilities extends Capabilities{
     public @Nonnull Requirement getVolumeProductRequirement() throws InternalException, CloudException;
 
     /**
-     * Identifies to what degree auto-assignment of device ID is supported/required in this cloud.
+     * Identifies whether the device ID is required to be provided upon attach() call or else if the cloud will auto-assign one
+     * auto-assignment of device ID is
+     * supported/required in this cloud.
      * @return whether device ID auto-assignment is required/optional/non-required by this cloud.
      * @throws InternalException an error occurred in the Dasein Cloud implementation determining the support level
      * @throws CloudException an error occurred with the cloud provider determining the support level
+     * @see org.dasein.cloud.compute.VolumeSupport#attach(String, String, String)
      */
-    public @Nonnull Requirement getVolumeDeviceIDAutoAssignRequirement() throws InternalException, CloudException;
+    public @Nonnull Requirement getDeviceIdOnAttachRequirement() throws InternalException, CloudException;
 
     /**
      * Indicates that a volume size is not necessary (and ultimately ignored) during the volume creation process
